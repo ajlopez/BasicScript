7bf613f709cf7eeb68cabdaa2b16042af2c91d77
ajlopez <webmaster@ajlopez.com>
2012-10-06 17:45:48 -0300

    Lexer parse name with tests

--- test/lexer.js
+++ test/lexer.js
@@ -55,3 +55,85 @@ assert.equal(TokenType.String, token.type);
 
 assert.equal(null, lexer.nextToken());
 
+// Parse two integers
+
+var lexer = new basicscript.Lexer("123 456");
+var token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal(123, token.value);
+assert.equal(TokenType.Integer, token.type);
+
+token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal(456, token.value);
+assert.equal(TokenType.Integer, token.type);
+
+assert.equal(null, lexer.nextToken());
+
+// Parse name
+
+var lexer = new basicscript.Lexer("a");
+var token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal("a", token.value);
+assert.equal(TokenType.Name, token.type);
+
+assert.equal(null, lexer.nextToken());
+
+// Parse name with upper case letters
+
+var lexer = new basicscript.Lexer("FOO");
+var token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal("FOO", token.value);
+assert.equal(TokenType.Name, token.type);
+
+assert.equal(null, lexer.nextToken());
+
+// Parse name with mixed case letters
+
+var lexer = new basicscript.Lexer("Foo");
+var token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal("Foo", token.value);
+assert.equal(TokenType.Name, token.type);
+
+assert.equal(null, lexer.nextToken());
+
+// Parse name with digits
+
+var lexer = new basicscript.Lexer("foo123");
+var token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal("foo123", token.value);
+assert.equal(TokenType.Name, token.type);
+
+assert.equal(null, lexer.nextToken());
+
+// Parse name with underscore at middle
+
+var lexer = new basicscript.Lexer("foo_bar");
+var token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal("foo_bar", token.value);
+assert.equal(TokenType.Name, token.type);
+
+assert.equal(null, lexer.nextToken());
+
+// Parse name with underscore at start
+
+var lexer = new basicscript.Lexer("_foo");
+var token = lexer.nextToken();
+
+assert.ok(token);
+assert.equal("_foo", token.value);
+assert.equal(TokenType.Name, token.type);
+
+assert.equal(null, lexer.nextToken());
