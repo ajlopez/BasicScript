3e66b7620f94af4c704191a797e77c0772e3412d
ajlopez <webmaster@ajlopez.com>
2012-10-06 17:01:28 -0300

    Evaluate string tests

--- test/evaluate.js
+++ test/evaluate.js
@@ -14,3 +14,15 @@ assert.equal(null, basicscript.evaluate(''));
 
 assert.equal(null, basicscript.evaluate('  '));
 
+// Evaluate integer
+
+assert.equal(123, basicscript.evaluate('123'));
+
+// Evaluate string delimited by single quotes
+
+assert.equal('foo', basicscript.evaluate("'foo'"));
+
+// Evaluate string delimited by double quotes
+
+assert.equal('foo', basicscript.evaluate('"foo"'));
+
