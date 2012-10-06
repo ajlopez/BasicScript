bb72210960fa5388023cfc5b6897fe805bc58876
ajlopez <webmaster@ajlopez.com>
2012-10-06 16:42:16 -0300

    Parse integer with test

--- test/parser.js
+++ test/parser.js
@@ -6,3 +6,11 @@ var parser = new basicscript.Parser();
 
 assert.ok(parser);
 
+// Parse integer expression
+
+var parser = new basicscript.Parser('123');
+var expression = parser.parseExpression();
+
+assert.ok(expression);
+assert.equal(123, expression.value);
+assert.equal(123, expression.evaluate());
