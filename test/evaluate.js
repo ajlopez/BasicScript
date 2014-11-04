e059eec19b6b4c059b48f590f76fed409f2cddaa
ajlopez <webmaster@ajlopez.com>
2014-11-04 08:03:39 -0300

    Refactor evaluate test

--- test/evaluate.js
+++ test/evaluate.js
@@ -4,37 +4,37 @@ var basicscript = require('../');
 var context;
 
 exports['evaluate null'] = function (test) {
-    test.equal(null, basicscript.evaluate(null));
+    test.equal(basicscript.evaluate(null), null);
 }
 
 exports['evaluate empty string'] = function (test) {
-    test.equal(null, basicscript.evaluate(''));
+    test.equal(basicscript.evaluate(''), null);
 }
 
 exports['evaluate spaces'] = function (test) {
-    test.equal(null, basicscript.evaluate('  '));
+    test.equal(basicscript.evaluate('  '), null);
 }
 
 exports['evaluate integer'] = function (test) { 
-    test.equal(123, basicscript.evaluate('123'));
+    test.equal(basicscript.evaluate('123'), 123);
 }
 
 exports['evaluate real'] = function (test) { 
-    test.equal(123.45, basicscript.evaluate('123.45'));
+    test.equal(basicscript.evaluate('123.45'), 123.45);
 }
 
 exports['evaluate string delimited by single quotes'] = function (test) {
-    test.equal('foo', basicscript.evaluate("'foo'"));
+    test.equal(basicscript.evaluate("'foo'"), 'foo');
 }
 
 exports['evaluate string delimited by double quotes'] = function (test) {
-    test.equal('foo', basicscript.evaluate('"foo"'));
+    test.equal(basicscript.evaluate('"foo"'), 'foo');
 }
 
 exports['evaluate undefined name'] = function (test) {
     context = basicscript.context();
 
-    test.equal(null, basicscript.evaluate('foo', context));
+    test.equal(basicscript.evaluate('foo', context), null);
 }
 
 exports['define values'] = function (test) {
@@ -42,47 +42,47 @@ exports['define values'] = function (test) {
     context.setValue('two', 2);
     context.setValue('three', 3);
 
-    test.equal(1, context.getValue('one'));
+    test.equal(context.getValue('one'), 1);
 }
 
 exports['evaluate defined name'] = function (test) {
-    test.equal(1, basicscript.evaluate('one', context));
+    test.equal(basicscript.evaluate('one', context), 1);
 }
 
 exports['evaluate add integers'] = function (test) {
-    test.equal(3, basicscript.evaluate('1+2'));
+    test.equal(basicscript.evaluate('1+2'), 3);
 }
 
 exports['evaluate add names'] = function (test) {
-    test.equal(3, basicscript.evaluate('one+two', context));
+    test.equal(basicscript.evaluate('one+two', context), 3);
 }
 
 exports['evaluate subtract integers'] = function (test) {
-    test.equal(-1, basicscript.evaluate('1-2'));
+    test.equal(basicscript.evaluate('1-2'), -1);
 }
 
 exports['evaluate subtract names'] = function (test) {
-    test.equal(-1, basicscript.evaluate('one-two', context));
+    test.equal(basicscript.evaluate('one-two', context), -1);
 }
 
 exports['evaluate multiply integers'] = function (test) {
-    test.equal(6, basicscript.evaluate('2*3'));
+    test.equal(basicscript.evaluate('2*3'), 6);
 }
 
 exports['evaluate multiply names'] = function (test) {
-    test.equal(6, basicscript.evaluate('two*three', context));
+    test.equal(basicscript.evaluate('two*three', context), 6);
 }
 
 exports['evaluate divide integers'] = function (test) {
-    test.equal(2/3, basicscript.evaluate('2/3'));
+    test.equal(basicscript.evaluate('2/3'), 2/3);
 }
 
 exports['evaluate divide integers'] = function (test) {
-    test.equal(2.1/3.2, basicscript.evaluate('2.1/3.2'));
+    test.equal(basicscript.evaluate('2.1/3.2'), 2.1/3.2);
 }
 
 exports['evaluate divide names'] = function (test) {
-    test.equal(2/3, basicscript.evaluate('two/three', context));
+    test.equal(basicscript.evaluate('two/three', context), 2/3);
 }
 
 exports['evaluate comparison operators'] = function (test) {
